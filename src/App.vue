1bfc47f30394f4bc2c23558378170cd7f69fdb87
Sebastiano <sebyguzz@icloud.com>
2023-05-11 01:37:58 +0200

    importo file

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <HeaderComponent />
+    <MainComponent />
+    <FooterComponent />
+  </div>
+</template>
+
+<script>
+import FooterComponent from './components/FooterComponent.vue';
+import HeaderComponent from './components/HeaderComponent.vue';
+import MainComponent from './components/MainComponent.vue';
+
+
+export default {
+  components: { HeaderComponent, MainComponent, FooterComponent }
+}
+</script>
+
+<style lang="scss" scoped></style>
